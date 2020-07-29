c32fa073b5867684b619f782721209e858349699
Firas Fatoum <ffatoum@CIT.internal>
2020-07-29 12:04:17 +0300

    test strings

---
+++ strings.py
@@ -0,0 +1,12 @@
+fname = 'Ali'
+lname = 'Ahmad'
+print(fname +' '+lname)
+
+print(fname.upper())
+print(lname.lower())
+
+fullname = 'Ali Ahmad Ali'
+print(fullname.replace('Ali','mohhamd'))
+
+rep = '$' * 10
+print(rep)
