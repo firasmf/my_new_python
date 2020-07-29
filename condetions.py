c114569c522febde139e95e544a7784a493b46a8
Firas Fatoum <ffatoum@CIT.internal>
2020-07-29 12:41:17 +0300

    condetion check user name and pass

---
+++ condetions.py
@@ -0,0 +1,7 @@
+username = input('Please Insert Your User Name: ')
+password = input('Please Insert Your Password: ')
+
+if(username == 'sam' and password == '123'):
+    print('Welcome Mr.'+username)
+else:
+    print('Error !!!')
