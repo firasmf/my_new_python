ed608c597c70e7985ccd6ea1882d10c620ef87eb
Firas Fatoum <ffatoum@CIT.internal>
2020-07-29 14:12:34 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+def line_fun():
+    print('-----------------------')
+
+
+def addtion(num1, num2):
+    print(int(num1) + int(num2))
+
+print('data 1')
+line_fun()
+print('data2')
+line_fun()
+addtion(20,30)
